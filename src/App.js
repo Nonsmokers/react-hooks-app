1f45439e42e01556c398c84553cf848c6e533c4c
Nonsmokers <Vasyuxnevichs@mail.ru>
2020-07-31 12:39:15 +0300

    fix: refactoring routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {Switch, Route, HashRouter} from 'react-router-dom';
 import {Home} from './pages/Home.jsx';
 import {Profile} from "./pages/Profile";
 import {About} from "./pages/About";
@@ -13,17 +13,17 @@ const App = () => {
     return (
         <GithubState>
             <AlertState>
-                <BrowserRouter>
+                <HashRouter basename={process.env.PUBLIC_URL}>
                     <Navbar/>
                     <div className="container pt-5">
                         <Alert alert={{text: 'test'}}/>
                         <Switch>
-                            <Route path='/' exact component={Home}/>
+                            <Route path='/' exact component={Home}  />
                             <Route path='/about' component={About}/>
                             <Route path='/profile/:name' component={Profile}/>
                         </Switch>
                     </div>
-                </BrowserRouter>
+                </HashRouter>
             </AlertState>
         </GithubState>
     )
